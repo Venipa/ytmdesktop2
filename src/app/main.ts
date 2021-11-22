7756924413e71a1a23235b4d464e460d89d41344
Venipa <admin@venipa.net>
2021-11-22 23:41:58 +0100

    updated to firefox ua, man i hate google

--- src/app/main.ts
+++ src/app/main.ts
@@ -78,6 +78,7 @@ export default async function() {
       },
       ...(options || {}),
     });
+    const stringifiedUA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0";
     win.webContents.session.webRequest.onBeforeSendHeaders(
       {
         urls: ["https://accounts.google.com/*"],
@@ -87,7 +88,7 @@ export default async function() {
           ...(details.requestHeaders || {}),
           ...{
             "User-Agent":
-              "Mozilla/5.0 (x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+              stringifiedUA,
           },
         };
         callback({ requestHeaders: newRequestHeaders });
