6bed23bd5f54fb97146af8b5e1a1f9e00624b793
Venipa <admin@venipa.net>
2024-11-05 00:28:31 +0100

    feat: updated builder, removed unused files being inside packaged installer

--- src/main/lib/store/createYmlStore.ts
+++ src/main/lib/store/createYmlStore.ts
@@ -1,8 +1,7 @@
-import { createEncryption } from '@shared/encryption'
-import slugify, { SlugifyOptions } from '@shared/slug'
+import { SlugifyOptions } from '@shared/slug'
 import { app } from 'electron'
 import Store, { Options } from 'electron-store'
-import { mkdirSync, readFileSync, statSync, writeFileSync } from 'node:fs'
+import { mkdirSync, statSync } from 'node:fs'
 import path from 'node:path'
 import { parse as deserialize, stringify as serialize } from 'yaml'
 const slugifyOptions = {
@@ -27,24 +26,3 @@ export const createYmlStore = <T extends Record<string, any> = Record<string, an
     deserialize,
     name
   })
-
-export const createEncryptedYmlStore = <T extends Record<string, any> = Record<string, any>>(
-  name: string,
-  options: Options<T> = {} as Options<T>
-) => {
-  const encryptionKeyPath = path.join(getStoreUserData(), slugify(name, slugifyOptions) + '.key')
-  const enc = createEncryption(name)
-  if (!statSync(encryptionKeyPath)) writeFileSync(encryptionKeyPath, enc.encrypt({ name }))
-  const encryptionKey = readFileSync(encryptionKeyPath).toString('utf8')
-  const payload = enc.decrypt<{ name: string }>(encryptionKey)
-  if (!payload || name !== payload?.name) throw new Error('Invalid encryption key')
-  return new Store<T>({
-    accessPropertiesByDotNotation: true,
-    fileExtension: "yml",
-    ...options,
-    name,
-    encryptionKey,
-    serialize,
-    deserialize
-  })
-}
--- src/shared/encryption/index.ts
+++
@@ -1,4 +0,0 @@
-import { default as Encryption } from "encryption.js";
-export const createEncryption = (secret: string) => (Encryption as any)({ secret }) as Encryption;
-export { };
-
