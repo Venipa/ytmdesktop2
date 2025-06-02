76dc3b444069be03ae5e9628ed89cce4bc6fa2f0
Venipa <admin@venipa.net>
2025-06-03 00:34:33 +0200

    fix: null chain join

--- src/main/utils/electron.ts
+++ src/main/utils/electron.ts
@@ -32,9 +32,9 @@ const fsLogger = (() => {
 		writeStream.write(`[unhandledRejection]: ${reason}\n${promise}\n`);
 	});
 	const allowedLevels = [LogLevel.Error, LogLevel.Warning];
-	return (source: string, level: LogLevel, objects: any[]) => {
+	return (source: string, level: LogLevel, objects: any[] = []) => {
 		if (!allowedLevels.includes(level)) return;
-		writeStream.write(`[${source}][${level}]: ${objects.join(" ")}\n`, () => {
+		writeStream.write(`[${source}][${level}]: ${objects.join(" ") ?? ""}\n`, () => {
 			if (writeStream.writableEnded) {
 				writeStream.end();
 			}
