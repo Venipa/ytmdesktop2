7416e1e00c79e3e1be9663b4c4eff1761090a030
Venipa <admin@venipa.net>
2025-06-29 20:05:25 +0200

    refactor: simplify Electron fuses configuration in afterPack script
    Signed-off-by: Venipa <admin@venipa.net>

--- scripts/afterPack.js
+++ scripts/afterPack.js
@@ -4,15 +4,12 @@ const { Arch } = require("electron-builder");
 exports.default = async function (context) {
 	const fuses = {
 		version: FuseVersion.V1,
-		strictlyRequireAllFuses: true,
 		[FuseV1Options.RunAsNode]: false,
 		[FuseV1Options.EnableCookieEncryption]: true,
 		[FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
 		[FuseV1Options.EnableNodeCliInspectArguments]: false,
 		[FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
 		[FuseV1Options.OnlyLoadAppFromAsar]: true,
-		[FuseV1Options.LoadBrowserProcessSpecificV8Snapshot]: true,
-		[FuseV1Options.GrantFileProtocolExtraPrivileges]: true,
 		resetAdHocDarwinSignature: context.electronPlatformName === "darwin" && context.arch === Arch.arm64,
 	};
 	await context.packager.addElectronFuses(context, fuses);
