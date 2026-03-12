687647a592c7c87ae20326b43df20330ef1bc0d0
Venipa <admin@venipa.net>
2026-03-12 12:05:05 +0100

    feat: updated notarize pack

--- scripts/afterPack.js
+++ scripts/afterPack.js
@@ -1,6 +1,10 @@
 const { FuseVersion, FuseV1Options } = require("@electron/fuses");
 const { Arch } = require("electron-builder");
-
+const { notarize } = require("@electron/notarize");
+/**
+ *
+ * @param {import("electron-builder").AfterPackContext} context
+ */
 exports.default = async function (context) {
 	const fuses = {
 		version: FuseVersion.V1,
@@ -13,4 +17,20 @@ exports.default = async function (context) {
 		resetAdHocDarwinSignature: context.electronPlatformName === "darwin" && context.arch === Arch.arm64,
 	};
 	await context.packager.addElectronFuses(context, fuses);
+	if (context.electronPlatformName === "darwin") {
+		const requiredEnvVars = ["APPLE_API_KEY", "APPLE_API_ISSUER", "APPLE_API_KEY_ID"];
+		for (const envVar of requiredEnvVars) {
+			if (!process.env[envVar]) throw new Error(`Apple Notarize failed, Missing environment variable: ${envVar}`);
+		}
+    const arch = context.arch === Arch.arm64 ? "arm64" : "x64";
+    const appPath = `dist/mac-${arch}/${context.packager.appInfo.productFilename}.app`;
+    console.log(`Notarizing ${appPath} for ${arch}...`);
+		await notarize({
+			appBundleId: context.packager.appInfo.productFilename,
+			appPath,
+			appleApiKey: process.env.APPLE_API_KEY,
+			appleApiIssuer: process.env.APPLE_API_ISSUER,
+			appleApiKeyId: process.env.APPLE_API_KEY_ID,
+		});
+	}
 };
