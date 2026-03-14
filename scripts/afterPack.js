2c2b554ef6736528c6236547a3a6ca2a329bc541
Venipa <admin@venipa.net>
2026-03-14 06:02:06 +0100

    fix: lru-cache mismatch issue in prod code

--- scripts/afterPack.js
+++ scripts/afterPack.js
@@ -19,52 +19,52 @@ exports.default = async function (context) {
 		resetAdHocDarwinSignature: context.electronPlatformName === "darwin" && context.arch === Arch.arm64,
 	};
 	await context.packager.addElectronFuses(context, fuses);
-	if (context.electronPlatformName === "darwin") {
-		const requiredEnvVars = ["APPLE_API_KEY", "APPLE_API_ISSUER", "APPLE_API_KEY_ID", "CSC_LINK", "CSC_KEY_PASSWORD"];
-		for (const envVar of requiredEnvVars) {
-			if (!process.env[envVar]) throw new Error(`Apple Notarize failed, Missing environment variable: ${envVar}`);
-		}
-		const appPath = `${context.outDir}/${context.arch === Arch.arm64 ? "mac-arm64" : "mac"}/${context.packager.appInfo.productFilename}.app`;
+	// if (context.electronPlatformName === "darwin") {
+	// 	const requiredEnvVars = ["APPLE_API_KEY", "APPLE_API_ISSUER", "APPLE_API_KEY_ID", "CSC_LINK", "CSC_KEY_PASSWORD"];
+	// 	for (const envVar of requiredEnvVars) {
+	// 		if (!process.env[envVar]) throw new Error(`Apple Notarize failed, Missing environment variable: ${envVar}`);
+	// 	}
+	// 	const appPath = `${context.outDir}/${context.arch === Arch.arm64 ? "mac-arm64" : "mac"}/${context.packager.appInfo.productFilename}.app`;
 
-		console.log(`\n\n\tSigning ${appPath} with certificate...`);
+	// 	console.log(`\n\n\tSigning ${appPath} with certificate...`);
 
-		const keychainProfile = process.env.APPLE_API_KEYCHAIN_PROFILE;
-		if (process.env.APPLE_API_ENV === "development") {
-			console.log(`\n\n\tStoring API key to notarytool profile...`);
-			spawnSync("xcrun", [
-				"notarytool",
-				"store-credentials",
-				keychainProfile,
-				"--key",
-				`${process.env.APPLE_API_KEY}`,
-				"--key-id",
-				process.env.APPLE_API_KEY_ID,
-				"--issuer",
-				process.env.APPLE_API_ISSUER,
-			]);
-		}
+	// 	const keychainProfile = process.env.APPLE_API_KEYCHAIN_PROFILE;
+	// 	if (process.env.APPLE_API_ENV === "development") {
+	// 		console.log(`\n\n\tStoring API key to notarytool profile...`);
+	// 		spawnSync("xcrun", [
+	// 			"notarytool",
+	// 			"store-credentials",
+	// 			keychainProfile,
+	// 			"--key",
+	// 			`${process.env.APPLE_API_KEY}`,
+	// 			"--key-id",
+	// 			process.env.APPLE_API_KEY_ID,
+	// 			"--issuer",
+	// 			process.env.APPLE_API_ISSUER,
+	// 		]);
+	// 	}
 
-		// keychain should already be unlocked
-		await signAsync({
-			app: appPath,
-			platform: "darwin",
-		});
-		console.log(`\tNotarizing ${appPath}\n\n`);
-		try {
-			await notarize({
-				appBundleId: context.packager.appInfo.productFilename,
-				keychainProfile,
-				appPath,
-			});
-		} catch (error) {
-			// if the error is not because the submission log is not yet available, throw the error
-			// notarisation will still be scanned by Apple, but we will not know the result until it is available
-			if (!error.message.includes("Submission log is not yet available")) {
-				console.error(`\tNotarization failed: ${error}`);
-				throw error;
-			} else {
-				console.info(`\tNotarization queued, will check later: \n${error.message}\n\n`);
-			}
-		}
-	}
+	// 	// keychain should already be unlocked
+	// 	await signAsync({
+	// 		app: appPath,
+	// 		platform: "darwin",
+	// 	});
+	// 	console.log(`\tNotarizing ${appPath}\n\n`);
+	// 	try {
+	// 		await notarize({
+	// 			appBundleId: context.packager.appInfo.productFilename,
+	// 			keychainProfile,
+	// 			appPath,
+	// 		});
+	// 	} catch (error) {
+	// 		// if the error is not because the submission log is not yet available, throw the error
+	// 		// notarisation will still be scanned by Apple, but we will not know the result until it is available
+	// 		if (!error.message.includes("Submission log is not yet available")) {
+	// 			console.error(`\tNotarization failed: ${error}`);
+	// 			throw error;
+	// 		} else {
+	// 			console.info(`\tNotarization queued, will check later: \n${error.message}\n\n`);
+	// 		}
+	// 	}
+	// }
 };
